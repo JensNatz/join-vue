be7f5cff5beb035816da3550b74e07e9557e35d6
Jens Natzschka <jens.natzschka@posteo.de>
2024-12-17 09:35:18 +0100

    first working prototype of draggable task board

--- src/components/organisms/TaskBoard.vue
+++ src/components/organisms/TaskBoard.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="task-board">
-        <div class="task-board-column" v-for="(column, status) in sortedTasks" :key="status" @dragover.prevent
-            @drop="onDrop(status)">
+        <div class="task-board-column" v-for="(column, status) in sortedTasks" :key="status">
             <div class="task-board-column-header">{{ status }}</div>
-            <div class="task-board-column-tasks" v-for="(task, index) in column" :key="index">
-                <TaskCard :task="task" :taskId="index" />
-            </div>
+            <VueDraggable class="task-board-column-tasks" v-model="sortedTasks[status]" group="tasks"
+                :item-key="'taskId'" :data-status="status" @update="onUpdate" @add="onAdd" @remove="onRemove">
+                <div v-for="task in column" :key="task.taskId">
+                    <TaskCard :task="task" :taskId="task.taskId" />
+                </div>
+            </VueDraggable>
         </div>
     </div>
 </template>
@@ -14,6 +16,7 @@ import { computed } from 'vue';
 import { useTasksStore } from '@/stores/tasks';
 import { useContactStore } from '@/stores/contact';
 import TaskCard from '../molecules/TaskCard.vue';
+import { VueDraggable } from 'vue-draggable-plus';
 
 const tasksStore = useTasksStore();
 const contactStore = useContactStore();
@@ -22,18 +25,32 @@ await Promise.all([
     contactStore.fetchContacts()
 ]);
 
-const sortedTasks = computed(() => tasksStore.getTasksSortedByStatus);
+const sortedTasks = computed(() => {
+    const tasks = tasksStore.getTasksSortedByStatus;
+    return Object.entries(tasks).reduce((acc, [status, tasksObj]) => {
+        acc[status] = Object.entries(tasksObj).map(([id, task]) => ({
+            ...task,
+            taskId: id
+        }));
+        return acc;
+    }, {});
+});
 
-const onDrop = async (newStatus) => {
-    const result = await tasksStore.updateTaskStatus(tasksStore.dragTaskId, newStatus);
-    tasksStore.setDragTaskId(null);
+function onUpdate(evt) {
+    console.log('update', evt.data.taskId, evt.oldIndex, evt.newIndex, evt.data.status);
+    tasksStore.updateTaskOrder(evt.data.taskId, evt.newIndex, evt.data.status);
+}
 
-    if (result.success) {
-        console.log("success");
-    } else {
-        console.log("error");
-    }
-};
+function onAdd(evt) {
+    const newStatus = evt.to.dataset.status;
+    const oldStatus = evt.from.dataset.status;
+    tasksStore.updateTaskOrder(evt.data.taskId, evt.newIndex, newStatus, evt.oldIndex, oldStatus);
+}
+
+function onRemove(evt) {
+    console.log('remove', evt.data.taskId);
+}
+
 </script>
 <style lang="scss">
 .task-board {
